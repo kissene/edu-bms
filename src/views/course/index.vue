868e01e872608afc391e4dd50505f6fd7bee798c
xietianliang <tsl-xietianliang@tasly.com>
2021-01-28 20:44:48 +0800

    feat: 添加基础文件

---
+++ src/views/course/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>CourseIndex</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CourseIndex'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>LoginIndex</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LoginIndex'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
