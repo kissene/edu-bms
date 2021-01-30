721c30cc9e652ab892f4678c86733f9b9525ae88
xietianliang <tsl-xietianliang@tasly.com>
2021-01-30 20:18:57 +0800

    feat: 添加layout文件及404页

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
-    <h1>Demo</h1>
     <!-- 根路由出口 -->
     <router-view />
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
-    <p class="text">hello world</p>
   </div>
 </template>
 
---
+++ src/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout">
+    <h1>头部</h1>
+    <h1>侧边栏</h1>
+
+    <!-- 子路由出口 -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LayoutIndex'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
